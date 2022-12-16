3539d95ac089756f881aff7dbfdff09ad521b7b5
aryan230 <aryan23062001@gmail.com>
2022-12-16 22:46:56 +0530

    third

--- server.js
+++ server.js
@@ -77,5 +77,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(notFound);
 app.use(errorHandler);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, console.log("Server running on port 5000"));
